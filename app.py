7906989b7b15f37b4439a1e71b04586ca895da6b
jaeho0718@naver.com <jaeho0718@naver.com>
2018-08-12 15:34:52 +0900

    'test'

--- app.py
+++ app.py
@@ -4,9 +4,24 @@ app = Flask(__name__)
 
 #We will receive messages that Facebook sends our bot at this endpoint
 @app.route("/webhook",methods=['POST'])
+def makeWebhookResult(req):
+    if req.get("result").get("action") != 'lunch':
+        return {}
+    result = req.get("result")
+    parameters = result.get("parameters")
+    zone = parameters.get("lunch")
+    speech = lunchparse(14)
+    print("Respose:")
+    print(speech)
+    return {
+        "speech":speech,
+        "displayText":speech,
+        "source":"clipai"
+    }
 def webhook():
     req = request.get_json(silent=True, force=True)
     res = makeWebhookResult(req)
+    res = json.dumps(res,indent=4)
     return "testsuccessful"
 
 if __name__ == "__main__":
