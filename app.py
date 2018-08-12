7d346a22fec6caa755f6150af8bf61fa5e268aff
jaeho0718@naver.com <jaeho0718@naver.com>
2018-08-12 15:33:21 +0900

    'test'

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 #We will receive messages that Facebook sends our bot at this endpoint
 @app.route("/webhook",methods=['POST'])
 def webhook():
+    req = request.get_json(silent=True, force=True)
+    res = makeWebhookResult(req)
     return "testsuccessful"
 
 if __name__ == "__main__":
